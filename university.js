a707fc951e3772f32fd0088723e2c0b715e94aa1
LukeTheCoder11 <tlqanimation@gmail.com>
2022-11-28 18:57:00 -0500

    university homework complete

---
+++ university.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $.get("http://universities.hipolabs.com/search?country=Canada", function(data,staus){
+        if (staus == "success" && data != null && data != undefined) {
+            for (let index = 0; index < data.length; index++) {
+                $("#universityList").append(`<a target="_blank" href="${data[index].web_pages[0]}" class="list-group-item list-group-item-action">${data[index].name}</a>`);
+            }
+        } else {
+            $("universityList").append("I sent out")
+        }
+    });
+});
+
